8c27ac13d84c69b2d8f90f4dd3dd67875218024d
aibanezm-esliceu <aibanezm@esliceu.net>
2026-05-29 16:01:47 +0200

    Inserció de codi a header.php

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="cat">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php echo isset($pageTitle) ? $pageTitle : 'Projecte de recuperació - Alejandro Ibañez Manzur'; ?></title>
+<link rel="stylesheet" href="/css/style.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="/css/normalize.css?v=<?php echo time(); ?>">
+</head>
+<body>
+<nav>
+<ul>
+<li><a href="index.php">Inici</a></li>
+<li><a href="contacte.php">Contacte</a></li>
+<li><a href="galeria.php">Galeria</a></li>
+</ul>
+</nav>
+<main>
