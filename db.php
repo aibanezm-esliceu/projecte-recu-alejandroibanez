51bcbfdbca4e51493840e955777dca0b77671954
aibanezm-esliceu <aibanezm@esliceu.net>
2026-05-29 17:25:10 +0200

    db.php4

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $user = 'recu';
 $user = 'recu123';
 $dbname = 'recu_db';
 
-$conn = new msqli($db, $recu, $recu123, $recu_db);
+$conn = new mysqli($db, $recu, $recu123, $recu_db);
 
 if($conn->connect_error) {
     die("Error de conexión: ". $conn->connect_error);
