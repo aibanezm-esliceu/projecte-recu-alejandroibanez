8ad4063f944f5dba2a5f17d28491e38cdfef4b71
aibanezm-esliceu <aibanezm@esliceu.net>
2026-05-29 16:14:30 +0200

    Inserció de dades a header.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = "Inicio - Examen PHP";
+require __DIR__ . "/includes/header.php";
+?>
+<section>
+<h1>Benvingut a la pagina d'inici d'aquest projecte</h1>
+<p>Aquest es el projecte de recuperació de examen.Footer i header carregats correctament</p>
+</section>
+<?php
+
+require __DIR__ . "/includes/footer.php";
+?>
