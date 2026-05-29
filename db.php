e54caa11c1af8cbe827803fb16ff5ccfea37fe96
aibanezm-esliceu <aibanezm@esliceu.net>
2026-05-29 17:20:03 +0200

    db.php3

--- db.php
+++ db.php
@@ -2,6 +2,7 @@
 try {
 $pdo = new PDO(
 "mysql:host=db;dbname=recu_db",
+$mysqli = new mysqli("db", "recu", "recu123", "recu_db");
 "recu",
 "recu123",
 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
